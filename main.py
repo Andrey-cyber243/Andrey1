bccf3051d29c2a122eb0e3fa4b6e3e40f4b1f9ca
Andrey-cyber243 <Sapuntsov64@mail.ru>
2024-09-24 09:57:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-example='Developer'
-print(example[0])
-print(example[-1])
-print(example[3:8])
-print(example[::-1])
-print(example[1:8:2])
+first_name='Курс:''Python'
+hwork_name=12# Количество выполненных ДЗ
+hours_name=1.5# Количество затраченных мин/час
+time_name=(12/90)#  время выполнения в минутах
+print (first_name, 'всего задач', hwork_name,'затрачено часов',hours_name,' время выполнения',time_name)
